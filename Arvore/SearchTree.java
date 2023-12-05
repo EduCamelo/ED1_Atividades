377ceb60090abec898da52459794ab28057d5f08
EduCamelo <eduardocamelobs@gmail.com>
2023-12-04 21:50:37 -0300

    Prova de ED1

--- Arvore/SearchTree.java
+++ Arvore/SearchTree.java
@@ -3,6 +3,26 @@ import java.util.Stack;
 public class SearchTree {
     private TreeB<Produto> raiz;
 
+ /*
+    ### Indicie das paradas ###
+    inserir recursivo: linha 30 e olhar classe TreeB
+    inserir não recursivo: 115
+    inserir repetidos e não recursivo:162  (obs: para a esquerda)
+    Passeio em ordem: 38
+    Passeio em ordem não recursivo: 144
+    Passeio pré-Ordem:55
+    Passeio pré-Ordem não recursivo:144
+    Passeio pós-Ordem:71
+    Passeio pós-Ordem não recursivo:80
+    Passeio Por Nível: 238
+    Busca:87 e olhar classe TreeB
+    Busca não recursiva:96
+    Busca de repetidos: FAZER
+    Remover: 209
+    Remover repetidos: FAZER
+  */
+
+
     public boolean isEmpty() {
         return raiz == null;
     }
@@ -15,19 +35,53 @@ public class SearchTree {
         }
     }
 
-    public void emOrdem(){
+    public void emOrdem() {// recursivo
         if (this.raiz == null) {
             System.out.println("Vazia!");
-        }else{
+        } else {
             this.percorrerEmOrdemRecursivo(raiz);
         }
 
     }
-    private void percorrerEmOrdemRecursivo(TreeB<Produto> r){
+
+    private void percorrerEmOrdemRecursivo(TreeB<Produto> r) {
         if (r != null) {
             percorrerEmOrdemRecursivo(r.getLeft());
             System.out.println(r.getInfo());
             percorrerEmOrdemRecursivo(r.getRight());
+        }
+    }
+
+    public void PreOrdem() {// recursivo
+        if (this.isEmpty()) {
+            System.out.println("Arvore vazia");
+        } else {
+            this.percorrerPreEmOrdemRecursivo(raiz);
+        }
+    }
+
+    private void percorrerPreEmOrdemRecursivo(TreeB<Produto> r) {
+        if (r != null) {
+            System.out.println(r.getInfo());
+            percorrerEmOrdemRecursivo(r.getLeft());
+            percorrerPreEmOrdemRecursivo(r.getRight());
+        }
+    }
+
+
+    public void posOrdem(){// recursivo
+        if(this.isEmpty()){
+            System.out.println("Arvore vazia");
+        }else{
+            this.percorrerEmPosOrdem(raiz);
+        }
+    }
+
+    private void percorrerEmPosOrdem(TreeB<Produto> r){
+        if(r!= null){
+            percorrerEmPosOrdem(r.getLeft());
+            percorrerEmPosOrdem(r.getRight());
+            System.out.println(r.getInfo());
         }
     }
 
@@ -37,35 +91,6 @@ public class SearchTree {
             return null;
         } else {
             return raiz.findNode(x);
-        }
-    }
-
-    public void alteraPreco(String cod, double preco) {
-        Produto aux = this.find(cod);
-        if (aux != null) {
-            aux.setPreco(preco);
-            System.out.println("Produto atualizado com sucesso!");
-        } else {
-            System.out.println("Produto não encontrado ou nã cadastrado");
-        }
-    }
-
-    public void alteraQtd(String cod, int qtd) {
-        Produto aux = this.find(cod);
-        if (aux != null) {
-            aux.setQtd(qtd);
-            System.out.println("Produto atualizado com sucesso!");
-        } else {
-            System.out.println("Produto não encontrado ou nã cadastrado");
-        }
-    }
-
-    public void exibirProd(String cod) {
-        Produto aux = this.find(cod);
-        if (aux != null) {
-            aux.toString();
-        } else {
-            System.out.println("Produto não encontrado");
         }
     }
 
@@ -116,80 +141,117 @@ public class SearchTree {
         }
     }
 
-    public TreeB<Produto> menor() {
-        if (this.isEmpty()) {
-            return null;
-        } else {
-            if (raiz.getLeft() == null) {
-                return raiz;
-            } else {
-                TreeB<Produto> aux = raiz;
-                do {
-                    aux = aux.getLeft();
-                } while (aux.getLeft() != null);
-                return aux;
-            }
-        }
-    }
-
-    public TreeB<Produto> maior() {
-        if (this.isEmpty()) {
-            return null;
-        } else {
-            if (raiz.getRight() == null) {
-                return raiz;
-            } else {
-                TreeB<Produto> aux = raiz;
-                do {
-                    aux = aux.getRight();
-                } while (aux.getRight() != null);
-                return aux;
-            }
-        }
-    }
-
-    public void emOrdemRecursivo(){
-        if(this.isEmpty()){
-            System.out.println("Lista vazia");
-        }else{
-            TreeB<Produto> aux;
-            Queue<Produto> fila = new Queue<Produto>();
-        }
-    }
-
-
-    public void preOrdemNaoRecursivo(){
+    public void preOrdemNaoRecursivo() {
         Stack<TreeB<Produto>> pilha = new Stack<TreeB<Produto>>();
         TreeB<Produto> aux = this.raiz;
-        if(this.isEmpty() == true){
+        if (this.isEmpty() == true) {
             System.out.println("Árvore vazia");
-        }else{
-            while(pilha.empty() == false || aux != null){
-                while(aux!= null){
+        } else {
+            while (!pilha.empty() || aux != null) {
+                while (aux != null) {
                     System.out.println(aux.getInfo().toString());
                     pilha.push(aux);
                     aux = aux.getLeft();
                 }
                 aux = pilha.pop();
-                aux = aux.getLeft();
+                aux = aux.getRight();
             }
         }
     }
 
-    public void OrdemNaoRecursivo(){
-        Stack<TreeB<Produto>> pilha = new Stack<TreeB<Produto>>();
-        TreeB<Produto> aux = this.raiz;
-        if(this.isEmpty() == true){
-            System.out.println("Árvore vazia");
-        }else{
-            while(!pilha.empty()|| aux != null){
-                while(aux!= null){
-                    pilha.push(aux);
-                    aux = aux.getLeft();
+    public void inserirSemRecursivoRepetido(Produto outro) { // para a esquerda
+        if (this.isEmpty()) {
+            raiz = new TreeB<Produto>(outro);
+        } else {
+            TreeB<Produto> aux = raiz;
+            do {
+                if (aux.getInfo().compareTo(outro) == 0) {
+                    if (aux.getLeft() == null) {
+                        aux.setLeft(new TreeB<Produto>(outro));
+                        break;
+                    } else {
+                        TreeB<Produto> aux1 = aux.getLeft();
+                        aux.setLeft(new TreeB<Produto>(outro));
+                        aux = aux.getLeft();
+                        aux.setLeft(aux1);
+                        break;
+                    }
+                } else {
+                    if (aux.getInfo().compareTo(outro) > 0) {
+                        if (aux.getLeft() == null) {
+                            aux.setLeft(new TreeB<Produto>(outro));
+                            break;
+                        } else {
+                            aux = aux.getLeft();
+                        }
+                    } else {
+                        if (aux.getRight() == null) {
+                            aux.setRight(new TreeB<Produto>(outro));
+                            break;
+                        } else {
+                            aux = aux.getRight();
+                        }
+                    }
                 }
-                aux = pilha.pop();
-                System.out.println(aux.getInfo().toString());
-                aux = aux.getRight();
+            } while (true);
+        }
+    }
+
+    public void remover(Produto value){
+        if(this.isEmpty()){
+            System.out.println("Arvore vazia");
+        }else{
+            raiz = this.removerNode(raiz,value);
+        }
+    }
+
+
+    private TreeB<Produto> removerNode(TreeB<Produto> r, Produto value){
+      if(r != null){
+        if(r.getInfo().compareTo(value) == 0){
+            if(r.getLeft() == null && r.getRight() == null){
+                r = null;
+            }else{
+                if(r.getLeft() == null){
+                    r = r.getRight();
+                }else if(r.getRight() == null){
+                    r = r.getLeft();
+                }else{
+                    TreeB<Produto> aux1=r, aux2=aux1.getLeft();
+                    while (aux2.getRight() != null) {
+                        aux1 = aux2;
+                        aux2 = aux2.getRight();
+                    }
+                    aux1.setRight(r.getLeft());
+                    r.setInfo(aux2.getInfo());
+                }
+            }
+        }else if(r.getInfo().compareTo(value)<0){
+            r.setLeft(removerNode(r.getLeft(), value));
+        }else{
+            r.setRight(removerNode(r.getRight(), value));
+        }
+      }
+      return r;
+    }
+
+    public void porNivel(){
+        Queue<TreeB<Produto>> fila =  new Queue<>();
+        TreeB<Produto> aux = raiz;
+
+        if (this.isEmpty()) {
+            System.out.println("Arvore Vazia");
+        }else{
+            fila.enqueue(aux);
+            while (!fila.empty()) {
+                aux = fila.dequeue();
+                if (aux.getLeft() != null) {
+                    fila.enqueue(aux.getLeft());
+                }
+                if (aux.getRight() != null) {
+                    fila.enqueue(aux.getRight());
+                }
+                System.out.println(aux.getInfo());
             }
         }
     }
--- Arvore/Teste.java
+++ Arvore/Teste.java
@@ -3,22 +3,24 @@ public class Teste {
         // Questão 1 - Teste do cadastro de produtos
         SearchTree arvoreProdutos = new SearchTree();
         // (a) Cadastrar um novo produto
-        Produto produto1 = new Produto("a", "Produto A", "Fornecedor A", 10.0, 100);
-        arvoreProdutos.inserirSemRecursivo(produto1);
+        Produto produto1 = new Produto("c", "200", "Fornecedor A", 10.0, 100);
+        arvoreProdutos.inserirSemRecursivoRepetido(produto1);
 
-        Produto produto2 = new Produto("b", "Produto B", "Fornecedor B", 20.0, 50);
-        arvoreProdutos.inserirSemRecursivo(produto2);
-        Produto produto3 = new Produto("c", "Produto C", "Fornecedor C", 30.0, 75);
-        arvoreProdutos.inserirSemRecursivo(produto3);
+        Produto produto2 = new Produto("b", "150", "Fornecedor B", 20.0, 50);
+        arvoreProdutos.inserirSemRecursivoRepetido(produto2);
+        Produto produto3 = new Produto("d", "250", "Fornecedor C", 30.0, 75);
+        arvoreProdutos.inserirSemRecursivoRepetido(produto3);
 
-        Produto produto4 = new Produto("d", "Produto A", "Fornecedor A", 10.0, 100);
-        arvoreProdutos.inserirSemRecursivo(produto4);
+        Produto produto4 = new Produto("f", "325", "Fornecedor A", 10.0, 100);
+        arvoreProdutos.inserirSemRecursivoRepetido(produto4);
 
-        Produto produto5 = new Produto("e", "Produto B", "Fornecedor B", 20.0, 50);
-        arvoreProdutos.inserirSemRecursivo(produto5);
+        Produto produto5 = new Produto("a", "100", "Fornecedor B", 20.0, 50);
+        arvoreProdutos.inserirSemRecursivoRepetido(produto5);
 
-         Produto produto6 = new Produto("f", "Produto C", "Fornecedor C", 30.0, 75);
-        arvoreProdutos.inserirSemRecursivo(produto6);
+         Produto produto6 = new Produto("e", "300", "Fornecedor C", 30.0, 75);
+        arvoreProdutos.inserirSemRecursivoRepetido(produto6);
+        Produto produto7 = new Produto("c", "199", "Fornecedor C", 30.0, 75);
+        arvoreProdutos.inserirSemRecursivoRepetido(produto7);
 
         arvoreProdutos.emOrdem();
     }
